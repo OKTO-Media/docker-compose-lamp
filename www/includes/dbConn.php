015c679165014737eb0f1f9b8ac35611d4265da5
Andy Low <42089621+AndyLow91@users.noreply.github.com>
2024-02-25 10:14:15 +0000

    Add files via upload

---
+++ www/includes/dbConn.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], $_ENV['MYSQL_DATABASE']);
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+// echo "Success: A proper connection to MySQL was made! The docker database is great." . PHP_EOL;
+
+mysqli_close($conn);
